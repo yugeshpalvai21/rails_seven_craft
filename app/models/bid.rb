64d20471c66171616241f8bd4b25f5e919adadd1
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-09-05 20:12:11 +0530

    updates validation on bid save along with auction association

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,5 +1,9 @@
 class Bid < ApplicationRecord
   belongs_to :auction
 
-  validates :amount, numericality: { greater_than: :current_price }
+  validates :amount, numericality: { greater_than: ->(bid) { bid.auction.current_price } }
+
+  after_create_commit do
+    auction.update!(current_price: amount)
+  end
 end
