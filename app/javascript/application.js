81e965e47b76053c72bd6c708407008b48fdb8d8
Yugesh Palvai <yugeshpalvai21@gmail.com>
2024-09-09 17:50:04 +0530

    feat: install lodash javascript library with importmap and explores basic usage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "channels"
+import _ from "lodash";
+
+
+// Example usage of lodash function
+const array = [1, 2, 3, 4];
+const revesed_array = _.reverse(array);
+console.log(revesed_array); // Output: [4, 3, 2, 1]
